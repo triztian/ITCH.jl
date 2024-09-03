c74aafc4e9ea74fcb3cc1cc76adf2cd14448c660
Tristian Azuara <tristian.azuara@gmail.com>
2024-09-03 02:53:23 -0400

    chore: minimal setup

--- src/TotalViewITCH.jl
+++ src/TotalViewITCH.jl
@@ -1,5 +1,13 @@
 module TotalViewITCH
 
-greet() = print("Hello World!")
+"""
+The version of the protocol being implemented.
+"""
+PROTOCOL_VERSION = 5.0
+
+struct Message 
+    messagetype::Char
+    timestamp::Int64
+end
 
 end # module TotalViewITCH
